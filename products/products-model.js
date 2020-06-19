435a4b352fb706cd5698d07d0a75d54dad960842
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-06-20 02:25:32 +0800

    built GET requests for categories and products endpoints, as well as POST request functionality for adding new products

---
+++ products/products-model.js
@@ -0,0 +1,43 @@
+const db = require("../data/db-config");
+
+function getCategories() {
+  return db("categories");
+}
+
+function getProducts(category_id) {
+  return db("products as p")
+    .join("categories as c", "c.id", "p.category_id")
+    .join("locations as l", "l.id", "p.location_id")
+    .join("users as u", "u.id", "p.seller_id")
+    .select(
+      "p.id",
+      "p.product_name as product",
+      "p.description",
+      "p.price",
+      "c.category_name as category",
+      "l.location_name as location",
+      "u.name as seller"
+    )
+    .where({ category_id });
+}
+
+async function addProduct(product) {
+  const [id] = await db("products").insert(product, "id");
+  return db("products")
+    .where({ id })
+    .first();
+}
+
+function findLocation(location) {
+  return db("locations")
+    .select("id")
+    .where({ location_name: location })
+    .first();
+}
+
+module.exports = {
+  getCategories,
+  getProducts,
+  addProduct,
+  findLocation
+};
